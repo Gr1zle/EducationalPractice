71fcc2a9fac9807fdf01c9f144d9a7487a34b1cc
Gr1zle <dmitriev.sv.dog@gmail.com>
2025-11-08 03:41:25 +0300

    Add files via upload

---
+++ FortniteShop2/fortshop/src/pages/Catalog.jsx
@@ -0,0 +1,59 @@
+import ProductCard from '../components/ProductCard';
+
+const fortniteItems = [
+  {
+    id: 1,
+    name: "Боевой пропуск",
+    price: 1000,
+    image: "https://avatars.mds.yandex.net/i?id=2a0000019a44ea2b00b9a7a33eab4bc54222-1392821-fast-images&n=13"
+  },
+  {
+    id: 2,
+    name: "Набор 'Темный рыцарь'",
+    price: 2000,
+    image: "https://i.pinimg.com/originals/4a/0c/c4/4a0cc4f060459a4686e4fa2f1d6408d7.jpg"
+  },
+  {
+    id: 3,
+    name: "Кирка 'Кайло минти'",
+    price: 1200,
+    image: "https://avatars.mds.yandex.net/i?id=ec790aebd9ced1ed6ff695a2d0ebd975_l-7751470-images-thumbs&n=13"
+  },
+  {
+    id: 4,
+    name: "Эмоция 'Признание поражения'",
+    price: 10000,
+    image: "https://www.dexerto.com/cdn-image/wp-content/uploads/2024/04/23/Fortnite-Take-The-L-emote-header-img.jpg?width=1200&quality=75&format=auto"
+  },
+  {
+    id: 5,
+    name: "Набор 'Айконик'",
+    price: 10000,
+    image: "https://avatars.mds.yandex.net/i?id=2ac3b1242d369a28ac0ab34ea9d87bcf_l-5084109-images-thumbs&n=13"
+  },
+  {
+    id: 6,
+    name: "Джем-трек 'd4vd'",
+    price: 500,
+    image: "https://i.scdn.co/image/ab67616d0000b273dbad5abeb49c5018ed65354b"
+  }
+];
+
+function Catalog() {
+  return (
+    <div className="main-content">
+      <header className="page-header">
+        <h1>Каталог товаров</h1>
+        <p>Выберите понравившиеся предметы</p>
+      </header>
+      
+      <div className="products-grid">
+        {fortniteItems.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Catalog;
